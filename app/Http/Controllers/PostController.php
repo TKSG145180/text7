efdf17224cf6381e333929b91d85bc48c8ae4911
TKSG145180 <zhuzhogzhiyu@gmail.com>
2023-09-29 21:50:20 +0900

    新しい

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use APP\Models\Post;
-
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+ return view('posts.index')->with(['posts' => $post->getBylimit()]);  
     }
 }
+
