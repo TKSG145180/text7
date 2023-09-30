cfbacc7f189276612bf525e3c26bbb20d3f2ea6a
TKSG145180 <zhuzhogzhiyu@gmail.com>
2023-09-30 10:56:26 +0900

    追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,12 +10,14 @@
     <body class="antialiased">
         <h1>Blog Name</h1>
         <div class='posts'>
-            @foreach ($posts as $post)
+            @foreach($posts as $post)
                <div class='post'>
                   <h2 class='title'>{{ $post->title }}</h2>
                   <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
-        </div>
+            </div>
+        <div class='paginate'>
+            {{ $posts->links() }}</div>
     </body>
 </html>
